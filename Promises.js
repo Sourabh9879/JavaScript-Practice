ae5026d67fcf7e894924652cc3a8a9eb02f0fa79
Sourabh9879 <kumarsourabh970@gmail.com>
2024-04-06 13:18:59 +0530

    promisees

--- Promises.js
+++ Promises.js
@@ -1,20 +1,47 @@
-// promises is used to solve callbackhell
-// promises > callback
-// three promises object 1) pending 2) rejected 3) fulfilled
+// // promises is used to solve callbackhell
+// // promises > callback
+// // three promises object 1) pending 2) rejected 3) fulfilled
 
-// let promise = new  Promise((resolve,reject) => {
-//     console.log("hello world")
-//     resolve(123)
+// // let promise = new  Promise((resolve,reject) => {
+// //     console.log("hello world")
+// //     resolve(123)
+// // })
+
+// const getPromise = () => {
+//    return new  Promise((resolve,reject) => {
+//         console.log("hello world")
+//         resolve("success")
+//         // reject(" ntwrk error")
+//     })
+// }
+
+// let promise = getPromise()
+// promise.then ((res) => {
+//     console.log("promise fulfilled",res)
 // })
 
-const getPromise = () => {
-   return new  Promise((resolve,reject) => {
-        console.log("hello world")
-        resolve("success")
+// promise.catch((err) => 
+// console.log("rejected",err))
+
+function asyncFunc1(){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            console.log("data1")
+            resolve("success")
+        }, 4000);
+    })
+}
+function asyncFunc2(){
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            console.log("data2")
+            resolve("success")
+        }, 4000);
     })
 }
 
-let promise = getPromise()
-promise.then (() => {
-    console.log("promise fulfilled")
+console.log("fetching data1.....")
+asyncFunc1().then((res) => {
+    console.log("fetching data2.....")
+    asyncFunc2().then((res) => {})
 })
