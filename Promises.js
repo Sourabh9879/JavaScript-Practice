30e78ee2fa603d8c830276d8a2dd29952a2fb9ed
Sourabh9879 <kumarsourabh970@gmail.com>
2024-04-06 12:14:56 +0530

    promisees

--- Promises.js
+++ Promises.js
@@ -1,7 +1,20 @@
 // promises is used to solve callbackhell
 // promises > callback
+// three promises object 1) pending 2) rejected 3) fulfilled
 
-let promise = new  Promise((resolve,reject) => {
-    console.log("hello world")
-    resolve(123)
+// let promise = new  Promise((resolve,reject) => {
+//     console.log("hello world")
+//     resolve(123)
+// })
+
+const getPromise = () => {
+   return new  Promise((resolve,reject) => {
+        console.log("hello world")
+        resolve("success")
+    })
+}
+
+let promise = getPromise()
+promise.then (() => {
+    console.log("promise fulfilled")
 })
